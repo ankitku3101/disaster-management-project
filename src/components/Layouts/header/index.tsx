85318023b8b06b6c09515a2907cf26250b865040
Adyasha Nanda <adyashananda105@gmail.com>
2025-04-16 18:05:06 +0530

    changes in header.tsx

--- src/components/Layouts/header/index.tsx
+++ src/components/Layouts/header/index.tsx
@@ -24,7 +24,13 @@ export function Header() {
 
       {isMobile && (
         <Link href={"/"} className="ml-2 max-[430px]:hidden min-[375px]:ml-4">
-          
+          <Image
+            src={"/images/logo/logo-icon.svg"}
+            width={32}
+            height={32}
+            alt=""
+            role="presentation"
+          />
         </Link>
       )}
 
