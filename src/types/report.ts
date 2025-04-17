b4d3d30c134014252ad3d47c865b88c2510e749a
royanish123 <anish.anandroy@gmail.com>
2025-04-17 07:53:05 +0530

    Added some parameter

--- src/types/report.ts
+++ src/types/report.ts
@@ -1,10 +1,11 @@
-export type UserReport = {
-    name: string;
-    disasterType: string;
-    location: string;
-    pincode: number;
-    description: string;
-    imageUrl?: string;
-    timestamp: string;
-  };
-  
+export interface UserReport {
+  _id: string;
+  name: string;
+  location: string;
+  pincode: number;
+  disasterType: string;
+  description: string;
+  imageUrl: string;
+  timestamp: string;
+  severity?: string;
+}
